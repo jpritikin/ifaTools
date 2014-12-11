6fd9ec573ec03459b06866147a2e604d94690796
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2014-12-11 08:58:34 -0500

    Improve EFA experience

--- R/model.R
+++ R/model.R
@@ -1,3 +1,97 @@
+#' Adds exploratory factors to a single factor model
+#'
+#' @param model a single factor (possibly multigroup) model
+#' @param toAdd the number of factors to add
+#' @param ...  Not used.  Forces remaining arguments to be specified by name.
+#' 
+#' @export
+addExploratoryFactors <- function(model, toAdd, ...) {
+    garbageArguments <- list(...)
+    if (length(garbageArguments) > 0) {
+        stop("Values for the '...' argument are invalid; use named arguments")
+    }
+
+  ex <- model$expectation
+  if (is(ex, "MxExpectationBA81")) {
+    if (any(sapply(ex$ItemSpec, function(s) s$factors) != 1)) {
+      stop(paste("Model", model$name, "has items that load",
+                 "on more or less than 1 factor"))
+    }
+    ex$ItemSpec <- lapply(ex$ItemSpec, rpf.modify, factors=1+toAdd)
+    item <- model[[ex$item]]
+    if (is.null(item)) {
+      stop(paste("Cannot find item matrix in model", model$name))
+    }
+    if (any(!is.na(item$lbound)) || any(!is.na(item$ubound))) {
+      stop(paste("EFA for item parameters with bounds are not implemented"))
+    }
+    nitem <- mxMatrix(nrow=nrow(item)+toAdd, ncol=ncol(item),
+                      name=item$name, dimnames=list(NULL, colnames(item)))
+    nitem$values[1,] <- item$values[1,]
+    nitem$values[(2+toAdd):nrow(nitem),] <- item$values[2:nrow(item),]
+    nitem$labels[1,] <- item$labels[1,]
+    nitem$labels[(2+toAdd):nrow(nitem),] <- item$labels[2:nrow(item),]
+    nitem$free[1,] <- item$free[1,]
+    nitem$free[(2+toAdd):nrow(nitem),] <- item$free[2:nrow(item),]
+    rownames(nitem) <- c(rownames(item)[1],
+                         paste0("explore", 1:toAdd),
+                         rownames(item)[2:nrow(item)])
+    
+    for (fx in 1:toAdd) {
+      lab <- paste0("E",fx)
+      mask <- item$free[1,]
+      mask[which(mask)[1:fx]] <- FALSE
+      nitem$free[1+fx,] <- mask
+      nitem$values[1+fx, mask] <- nitem$values[1, mask]
+      mask <- mask & !is.na(nitem$labels[1,])
+      nitem$labels[1+fx, mask] <- paste0(lab, nitem$labels[1, mask])
+    }
+    
+    ex$qpoints <- min(ex$qpoints,
+                      switch(as.character(1+toAdd),
+                             '2'=31, '3'=11, '4'=7, 5))
+    ex$qwidth <- min(ex$qwidth,
+                      switch(as.character(1+toAdd),
+                             '2'=5, '3'=4, '4'=3, 2))
+
+    model <- mxModel(model, ex, nitem)
+    
+    mMat <- model[[ex$mean]]
+    if (!is.null(mMat)) {
+      nmMat <- mxMatrix(nrow=1, ncol=1+toAdd, values=0, free=TRUE,
+                        name=mMat$name)
+      colnames(nmMat) <- c(ifelse(length(rownames(mMat)),
+                                  rownames(mMat), colnames(mMat)),
+                           paste0("explore", 1:toAdd))
+      # preserve labels? TODO
+      model <- mxModel(model, nmMat)
+    }
+
+    cMat <- model[[ex$cov]]
+    if (!is.null(cMat)) {
+      ncMat <- mxMatrix(type="Symm", nrow=1+toAdd, ncol=1+toAdd,
+                        values=diag(1+toAdd), free=TRUE,
+                        dimnames=list(colnames(nmMat), colnames(nmMat)),
+                        name=cMat$name)
+      for (rx in 1:toAdd) {
+        for (cx in (rx+1):(1+toAdd)) {
+          lab <- paste(model$name, cMat$name,
+                       paste0("r", rx, "c", cx), sep="_")
+          ncMat$labels[rx,cx] <-
+            ncMat$labels[cx,rx] <- lab
+        }
+      }
+      model <- mxModel(model, ncMat)
+    }
+  }
+ 
+  subs <- lapply(names(model$submodels),
+                 function(sname) {
+                   addExploratoryFactors(model[[sname]], toAdd)
+                 })
+  mxModel(model, subs)
+}
+
 #' Replicate a model for each group of data
 #'
 #' The reference group is fixed to a zero mean and identity covariance matrix.
@@ -10,10 +104,12 @@
 #' @param splitCol the name of the column used to indicate group membership
 #' @param refGroup the name of the reference group
 #' @param split whether to split the data (defaults to TRUE)
+#' @param compressData whether to apply compressDataFrame (defaults to TRUE)
 #' 
 #' @export
 replicateModelBy <- function(tmpl, fullData, mMat, covMat, ...,
-			     splitCol="population", refGroup="general", split=TRUE) {
+			     splitCol="population", refGroup="general", split=TRUE,
+			     compressData=TRUE) {
     garbageArguments <- list(...)
     if (length(garbageArguments) > 0) {
         stop("Values for the '...' argument are invalid; use named arguments")
@@ -21,6 +117,9 @@ replicateModelBy <- function(tmpl, fullData, mMat, covMat, ...,
 
   dataCol <- setdiff(colnames(fullData), splitCol)
   itemMatName <- tmpl$expectation$item
+    if (compressData) {
+	    tmpl$expectation$weightColumn <- 'freq'
+    }
   ivalues <- tmpl[[itemMatName]]$values
   newLabels <- tmpl[[itemMatName]]$labels
   
@@ -34,9 +133,15 @@ replicateModelBy <- function(tmpl, fullData, mMat, covMat, ...,
   tmpl[[itemMatName]]$labels <- newLabels
   
   refData <- subset(fullData, !split | population==refGroup, dataCol)
+    if (compressData) {
+	    refData <- compressDataFrame(refData)
+    }
   refModel <- mxModel(model=tmpl, name=refGroup,
-                      mxData(observed=refData, type="raw"))
-
+                      mxData(observed=refData, type="raw", sort=!compressData))
+    if (compressData) {
+	    refModel$data$numObs <- sum(refData[['freq']])
+    }
+    
   extraGroups <- setdiff(unique(fullData[[splitCol]]), refGroup)
   container <- mxModel(model="container", refModel)
   if (!split) {
@@ -48,8 +153,15 @@ replicateModelBy <- function(tmpl, fullData, mMat, covMat, ...,
   }
 
   for (grp in extraGroups) {
-    gmodel <- mxModel(model=tmpl, name=grp, mMat, covMat,
-                      mxData(observed=subset(fullData, population==grp, dataCol), type="raw"))
+	  gdata <- subset(fullData, population==grp, dataCol)
+	  if (compressData) {
+		  gdata <- compressDataFrame(gdata)
+	  }
+	  gmodel <- mxModel(model=tmpl, name=grp, mMat, covMat,
+			    mxData(observed=gdata, type="raw", sort=!compressData))
+	  if (compressData) {
+		  gmodel$data$numObs <- sum(gdata[['freq']])
+	  }
     lmodel <- mxModel(model=paste0(grp,"Latent"),
                       mxDataDynamic(type='cov', expectation=paste(grp,'expectation',sep='.')),
                       mxExpectationNormal(covariance=paste(grp,'cov',sep='.'),
--- inst/example/exploratoryIFA.R
+++ inst/example/exploratoryIFA.R
@@ -1,5 +1,7 @@
 library(OpenMx)
 library(rpf)
+library(ifaTools)
+library(GPArotation)
 
 # --------------------------------------------------------------- generate data
 
@@ -13,28 +15,57 @@ numItems <- 20
 spec <- list()  # a map from columns to item models
 
 # rpf.grm creates a graded response model
-spec[1:numItems] <- rpf.grm(factors=2)
+spec[1:numItems] <- rpf.grm(factors=2, outcomes=5)
 
-# randomly generate the true item parameters
-trueIMat <- mxSimplify2Array(lapply(spec, rpf.rparam))
-rownames(trueIMat)[1:2] <- factorNames
+trueIMat <- matrix(1.5, nrow=6, ncol=numItems)
+trueIMat[3,] <- seq(-2,2, length.out = numItems/2) + .5
+trueIMat[4,] <- seq(-2,2, length.out = numItems/2) + .1
+trueIMat[5,] <- seq(-2,2, length.out = numItems/2) - .1
+trueIMat[6,] <- seq(-2,2, length.out = numItems/2) - .5
+rownames(trueIMat) <- c(factorNames, paste0("b",1:4))
+colnames(trueIMat) <- paste0('i', 1:numItems)
+names(spec) <- colnames(trueIMat)
 
 # half the items mostly assess spelling and the other half of the items assess phonics
 trueDiscrimination0 <- apply(trueIMat[1:2,], 2, norm, "2")
-trueIMat['spelling',1:(numItems/2)] <- .1 * trueIMat['spelling',1:(numItems/2)]
-trueIMat['phonics',(1+numItems/2):numItems] <- .1 * trueIMat['phonics',(1+numItems/2):numItems]
+trueIMat['spelling',1:(numItems/2)] <- 0
+trueIMat['phonics',(1+numItems/2):numItems] <- 0
 
 # we need to rescale the intercept to keep it in the original place
-trueIMat['b',] <- apply(trueIMat[1:2,], 2, norm, "2") * trueIMat['b',] / trueDiscrimination0
-
-# set the true latent structure
-trueCovariance <- .2   # change this and see what happens
-trueLatentCov <- matrix(c(1, trueCovariance, trueCovariance, 1), 2, 2)
+trueIMat[3:6,] <- apply(trueIMat[1:2,], 2, norm, "2") * trueIMat[3:6,] / trueDiscrimination0
 
 # simulate some data
-data <- rpf.sample(2000, spec, trueIMat, cov=trueLatentCov)
+if (1) {  # hard
+  basis1 <- runif(2)
+  basis1 <- basis1 / norm(basis1, "2")
+  basis2 <- c()
+  for (retry in 1:10) {
+    basis2 <- runif(2)
+    basis2 <- basis2 / norm(basis2, "2")
+    if (basis1 %*% basis2 < .8) break
+  }
+  variance <- rlnorm(2)
+} else {  # easy
+  basis1 <- c(1,0)
+  basis2 <- c(0,1)
+  variance <- c(1,1) #rlnorm(2)
+}
+
+theta <- rbind(t(as.matrix(basis1) %*% rnorm(1000, sd=sqrt(variance[1]))),
+               t(as.matrix(basis2) %*% rnorm(1000, sd=sqrt(variance[2]))))
+#theta <- t(as.matrix(basis1) %*% rnorm(1000, sd=sqrt(variance[1])))
+data <- rpf.sample(t(theta), spec, trueIMat)
 itemNames <- colnames(data)
 data <- compressDataFrame(data)
+
+if (0) {
+  # Note severe signal degredation even using the true item parameters!
+  plot(theta)
+  tgrp <- list(spec=spec, param=trueIMat, data=data, weightColumn="freq")
+  require(ggplot2)
+  sc <- as.data.frame(EAPscores(tgrp)[,1:2])
+  ggplot(sc, aes(x=spelling, y=phonics)) + geom_point(size=3, alpha=.15)
+}
 
 # --------------------------------------------------------------- 1 factor
 
@@ -44,9 +75,10 @@ computePlan <- mxComputeSequence(list(
   mxComputeHessianQuality(),
   mxComputeStandardError()))
 
-spec[1:numItems] <- rpf.grm()
-imat <- mxMatrix(name="item", nrow=2, ncol=numItems, values=c(1,0), free=TRUE,
-                 dimnames=list(c("lang","b"), itemNames))
+spec[1:numItems] <- lapply(spec, rpf.modify, factors=1)
+
+imat <- mxMatrix(name="item", free=TRUE,
+                 values=mxSimplify2Array(lapply(spec, rpf.rparam)))
 
 m1 <- mxModel(model="model1", imat,
               mxData(observed=data, type="raw", numObs=sum(data$freq)),
@@ -54,43 +86,64 @@ m1 <- mxModel(model="model1", imat,
               mxFitFunctionML(),
               computePlan)              
 m1 <- mxRun(m1)
+m1G <- as.IFAgroup(m1)
 
 # --------------------------------------------------------------- 2 factors
 
-spec[1:numItems] <- rpf.grm(factors = 2)
-imat <- mxMatrix(name="item", nrow=3, ncol=numItems, values=c(1,1,0), free=TRUE,
-                 dimnames=list(c(factorNames,"b"), itemNames))
-
-# for identification, we need to fix 1 slope to 0
-imat$values['spelling','i1'] <- 0
-imat$free['spelling','i1'] <- FALSE
-
-m2 <- mxModel(model="model2", imat,
-              mxData(observed=data, type="raw", numObs=sum(data$freq)),
-              mxExpectationBA81(ItemSpec=spec, weightColumn = "freq",
-                                qpoints=31, qwidth=5),  # speed things up with fewer quadrature pts
-              mxFitFunctionML(),
-              computePlan)              
+m2 <- addExploratoryFactors(m1, 1)
 m2 <- mxRun(m2)
+m2G <- as.IFAgroup(m2)
+
+# --------------------------------------------------------------- 3 factors
+
+m3 <- addExploratoryFactors(m1, 2)
+m3 <- mxRun(m3)
+m3G <- as.IFAgroup(m3)
 
 # --------------------------------------------------------------- compare fit
 
-mxCompare(m2,m1)  # likelihood ratio test
+m0 <- mxRefModels(m1, run=TRUE)
 
-# i12 has huge SEs so we can expect to see some bad stuff in fit diagnostics
+mxCompare(m3,c(m2,m1,m0$Independence))  # likelihood ratio test
 
-# prepare IFAgroup objects for further diagnostics
-m1G <- as.IFAgroup(m1)
-m2G <- as.IFAgroup(m2)
+if (0) {
+  
+  m2G$param[1:2,] <- m2$item$values[1:2,]
+  plot(EAPscores(m2G)[,1:2])
+  m2G$param[1:2,] <- fromFactorLoading(oblimin(toFactorLoading(m2$item$values[1:2,]))$loadings)
+  plot(EAPscores(m2G)[,1:2])
+  m2G$param[1:2,] <- fromFactorLoading(geominT(toFactorLoading(m2$item$values[1:2,]))$loadings)
+  plot(EAPscores(m2G)[,1:2])
+  m2G$param[1:2,] <- fromFactorLoading(bentlerQ(toFactorLoading(m2$item$values[1:2,]))$loadings)
+  plot(EAPscores(m2G)[,1:2])
+  m2G$param[1:2,] <- fromFactorLoading(bentlerT(toFactorLoading(m2$item$values[1:2,]))$loadings)
+  plot(EAPscores(m2G)[,1:2])
+  m2G$param[1:2,] <- fromFactorLoading(simplimax(toFactorLoading(m2$item$values[1:2,]))$loadings)
+  plot(EAPscores(m2G)[,1:2])
+  m2G$param[1:2,] <- fromFactorLoading(cfQ(toFactorLoading(m2$item$values[1:2,]))$loadings)
+  plot(EAPscores(m2G)[,1:2])
+  #             h2 <- rowSums(Flist$F^2)
+}
 
-# i12 indeed shows up as unusual
-SitemFit(m1G)
-SitemFit(m2G)
+if (0) {
+  SitemFit(m1G)
+  SitemFit(m2G)
+  
+  # We don't trust these tests, but here's how to do it
+  multinomialFit(m1G)
+  multinomialFit(m2G)
+  
+  ChenThissen1997(m1G)
+  ChenThissen1997(m2G)
+}
 
-# We don't trust these tests, but here's how to do it
-multinomialFit(m1G)
-multinomialFit(m2G)
-
-# fit is so bad, these are unhelpful
-ChenThissen1997(m1G)
-ChenThissen1997(m2G)
+if (0) {
+  require("mirt")
+  mdata <- expandDataFrame(as.data.frame(lapply(data, unclass)), freqName = "freq")
+  mod <- mirt(mdata, 2, TOL=c(.001, .01))
+  summary(mod, rotate="none")
+  
+  clist <- coef(mod)
+  clist$GroupPars <- NULL
+  FL <- toFactorLoading(mxSimplify2Array(clist)[1:2,])
+}
